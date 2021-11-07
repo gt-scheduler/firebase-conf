69a3fc10e3c1bf6f3de33ba46c15d86184ee9075
Joseph Azevedo <joseph.az@gatech.edu>
2021-11-06 20:40:56 -0400

    add CORS support to cache function

--- functions/src/course_critique_cache.ts
+++ functions/src/course_critique_cache.ts
@@ -5,6 +5,7 @@ import * as zlib from "zlib";
 import * as util from "util";
 import { Response } from "express";
 import admin from "./firebase";
+import * as cors from "cors";
 import { apiError } from "./api";
 
 const compress = util.promisify(zlib.brotliCompress);
@@ -39,124 +40,134 @@ type CacheItem = {
   c: string;
 };
 
+// Right now, this always allows the requesting origin.
+// Nothing sensitive is served from this function,
+// so this is fine to use.
+// Not restricting the origin to gt-scheduler.org
+// makes it easier to develop locally by using the same API endpoint.
+const corsHandler = cors({ origin: true });
+
 export const getCourseDataFromCourseCritique = functions.https.onRequest(
-  async (request, response) => {
-    // Get the course ID from the request
-    const courseID = request.query["courseID"];
-    if (courseID == null || courseID.length === 0) {
-      response
-        .status(400)
-        .json(apiError("request missing 'courseID' query parameter"));
-      return;
-    } else if (typeof courseID !== "string") {
-      response
-        .status(400)
-        .json(
-          apiError("request should contain a single 'courseID' query parameter")
-        );
-      return;
-    } else if (courseID.length > 16) {
-      // No valid course IDs are longer than 16 characters
-      // (just place some upper limit on them).
-      response
-        .status(400)
-        .json(
-          apiError(
-            "'courseID' query parameter is too long (must be <= 16 characters)"
-          )
-        );
-      return;
-    }
+  (request, response) =>
+    corsHandler(request, response, async () => {
+      // Get the course ID from the request
+      const courseID = request.query["courseID"];
+      if (courseID == null || courseID.length === 0) {
+        response
+          .status(400)
+          .json(apiError("request missing 'courseID' query parameter"));
+        return;
+      } else if (typeof courseID !== "string") {
+        response
+          .status(400)
+          .json(
+            apiError(
+              "request should contain a single 'courseID' query parameter"
+            )
+          );
+        return;
+      } else if (courseID.length > 16) {
+        // No valid course IDs are longer than 16 characters
+        // (just place some upper limit on them).
+        response
+          .status(400)
+          .json(
+            apiError(
+              "'courseID' query parameter is too long (must be <= 16 characters)"
+            )
+          );
+        return;
+      }
 
-    // Check to see if the course has a cached value in Firestore
-    let maybeStaleCache: CacheItem | null = null;
-    const document = await courseDataCacheCollection.doc(courseID).get();
-    if (document.exists) {
-      const data = document.data();
-      if (data != null) {
-        // Use a different expiration if the cached response is an error
-        let expirationSeconds: number;
-        if (data.s >= 400) {
-          expirationSeconds = CACHE_ERROR_RESPONSE_EXPIRATION_SECONDS;
-        } else {
-          expirationSeconds = CACHE_SUCCESS_RESPONSE_EXPIRATION_SECONDS;
+      // Check to see if the course has a cached value in Firestore
+      let maybeStaleCache: CacheItem | null = null;
+      const document = await courseDataCacheCollection.doc(courseID).get();
+      if (document.exists) {
+        const data = document.data();
+        if (data != null) {
+          // Use a different expiration if the cached response is an error
+          let expirationSeconds: number;
+          if (data.s >= 400) {
+            expirationSeconds = CACHE_ERROR_RESPONSE_EXPIRATION_SECONDS;
+          } else {
+            expirationSeconds = CACHE_SUCCESS_RESPONSE_EXPIRATION_SECONDS;
+          }
+
+          // See if the document's addition time is fresh enough
+          const now = new Date();
+          const expiredAt = date.addSeconds(now, expirationSeconds);
+          const timestamp = new Date(data.t);
+          if (timestamp < expiredAt) {
+            functions.logger.info("Using cached data for course", {
+              course_id: courseID,
+            });
+
+            await sendCachedResponse(data, response);
+            return;
+          }
+
+          // Store the data to use in case there is an error contacting upstream
+          maybeStaleCache = data;
         }
+      }
 
-        // See if the document's addition time is fresh enough
-        const now = new Date();
-        const expiredAt = date.addSeconds(now, expirationSeconds);
-        const timestamp = new Date(data.t);
-        if (timestamp < expiredAt) {
+      // Fetch the upstream data
+      const encodedCourseID = encodeURIComponent(courseID);
+      const url = `${UPSTREAM_COURSE_DATA_URL}?courseID=${encodedCourseID}`;
+      let upstreamResponse: fetch.Response;
+      let upstreamResponseBody: string;
+      try {
+        upstreamResponse = await fetch.default(url, { method: "GET" });
+        upstreamResponseBody = await upstreamResponse.text();
+      } catch (err) {
+        // There was an error contacting the upstream API
+        // (this is not a 3xx-5xx response code).
+        // Log the error
+        functions.logger.error(
+          `An error occurred while making a request to Course Critique: ${err}`,
+          { url }
+        );
+
+        // If there is cached data (even if stale), return it.
+        if (maybeStaleCache !== null) {
           functions.logger.info("Using cached data for course", {
             course_id: courseID,
           });
 
-          await sendCachedResponse(data, response);
+          await sendCachedResponse(maybeStaleCache, response);
           return;
         }
 
-        // Store the data to use in case there is an error contacting upstream
-        maybeStaleCache = data;
-      }
-    }
-
-    // Fetch the upstream data
-    const encodedCourseID = encodeURIComponent(courseID);
-    const url = `${UPSTREAM_COURSE_DATA_URL}?courseID=${encodedCourseID}`;
-    let upstreamResponse: fetch.Response;
-    let upstreamResponseBody: string;
-    try {
-      upstreamResponse = await fetch.default(url, { method: "GET" });
-      upstreamResponseBody = await upstreamResponse.text();
-    } catch (err) {
-      // There was an error contacting the upstream API
-      // (this is not a 3xx-5xx response code).
-      // Log the error
-      functions.logger.error(
-        `An error occurred while making a request to Course Critique: ${err}`,
-        { url }
-      );
-
-      // If there is cached data (even if stale), return it.
-      if (maybeStaleCache !== null) {
-        functions.logger.info("Using cached data for course", {
-          course_id: courseID,
-        });
-
-        await sendCachedResponse(maybeStaleCache, response);
+        // Otherwise, return a 504 (Gateway Timeout)
+        response
+          .status(504)
+          .json(apiError("could not contact upstream Course Critique API"));
         return;
       }
 
-      // Otherwise, return a 504 (Gateway Timeout)
+      // Store the timestamp, data, and status code in Firestore
+      const timestamp = new Date().toISOString();
+      const contentType =
+        upstreamResponse.headers.get("content-type") ?? "application/json";
+      const writePromise = storeCacheItem(courseID, upstreamResponseBody, {
+        v: 1,
+        t: timestamp,
+        s: upstreamResponse.status,
+        c: contentType,
+      });
+
+      functions.logger.info("Loaded cache with fresh data for course", {
+        course_id: courseID,
+      });
+
+      // Eagerly send the response before waiting for the write to complete
       response
-        .status(504)
-        .json(apiError("could not contact upstream Course Critique API"));
-      return;
-    }
+        .status(upstreamResponse.status)
+        .header("Content-Type", contentType)
+        .send(upstreamResponseBody);
 
-    // Store the timestamp, data, and status code in Firestore
-    const timestamp = new Date().toISOString();
-    const contentType =
-      upstreamResponse.headers.get("content-type") ?? "application/json";
-    const writePromise = storeCacheItem(courseID, upstreamResponseBody, {
-      v: 1,
-      t: timestamp,
-      s: upstreamResponse.status,
-      c: contentType,
-    });
-
-    functions.logger.info("Loaded cache with fresh data for course", {
-      course_id: courseID,
-    });
-
-    // Eagerly send the response before waiting for the write to complete
-    response
-      .status(upstreamResponse.status)
-      .header("Content-Type", contentType)
-      .send(upstreamResponseBody);
-
-    await writePromise;
-  }
+      await writePromise;
+    })
 );
 
 async function sendCachedResponse(item: CacheItem, response: Response) {

e77ad44cd86a7b8b0f55acd069451be9daee03df
Joseph Azevedo <joseph.az@gatech.edu>
2021-11-08 19:27:17 -0500

    add global in-memory process cache, prepare to turn logs to metrics

--- functions/src/course_critique_cache.ts
+++ functions/src/course_critique_cache.ts
@@ -8,9 +8,6 @@ import admin from "./firebase";
 import * as cors from "cors";
 import { apiError } from "./api";
 
-const compress = util.promisify(zlib.brotliCompress);
-const decompress = util.promisify(zlib.brotliDecompress);
-
 const UPSTREAM_COURSE_DATA_URL =
   "https://c4citk6s9k.execute-api.us-east-1.amazonaws.com/prod/data/course";
 
@@ -47,6 +44,11 @@ type CacheItem = {
 // makes it easier to develop locally by using the same API endpoint.
 const corsHandler = cors({ origin: true });
 
+// Optimization: provide a global in-memory cache for this process
+// that can be used across multiple invocations
+// that get scheduled on the same process
+const globalCache: Record<string, CacheItem> = {};
+
 export const getCourseDataFromCourseCritique = functions.https.onRequest(
   (request, response) =>
     corsHandler(request, response, async () => {
@@ -79,57 +81,94 @@ export const getCourseDataFromCourseCritique = functions.https.onRequest(
         return;
       }
 
-      // Check to see if the course has a cached value in Firestore
-      let maybeStaleCache: CacheItem | null = null;
-      const document = await courseDataCacheCollection.doc(courseID).get();
-      if (document.exists) {
-        const data = document.data();
-        if (data != null) {
-          // Use a different expiration if the cached response is an error
-          let expirationSeconds: number;
-          if (data.s >= 400) {
-            expirationSeconds = CACHE_ERROR_RESPONSE_EXPIRATION_SECONDS;
-          } else {
-            expirationSeconds = CACHE_SUCCESS_RESPONSE_EXPIRATION_SECONDS;
-          }
+      // Optimization: check to see if the course has a cached value
+      // in the in-memory cache
+      const globalCacheItem = getCacheItemFromGlobalCache(courseID);
+      if (globalCacheItem !== null) {
+        const age = getCacheItemAge(globalCacheItem);
+        if (age < getCacheItemExpirationSeconds(globalCacheItem)) {
+          // Use this log line to construct custom metrics
+          // so we can observe how often the global cache is used:
+          // https://cloud.google.com/logging/docs/logs-based-metrics#user-metrics
+          functions.logger.info("Using globally cached data for course", {
+            course_id: courseID,
+            age,
+          });
 
-          // See if the document's addition time is fresh enough
-          const now = new Date();
-          const expiredAt = date.addSeconds(now, expirationSeconds);
-          const timestamp = new Date(data.t);
-          if (timestamp < expiredAt) {
-            await sendCachedResponse(courseID, response, data);
-            return;
-          }
+          await sendCachedResponse(response, {
+            data: await decompressBody(globalCacheItem.d),
+            contentType: globalCacheItem.c,
+            status: globalCacheItem.s,
+            age,
+          });
 
-          // Store the data to use in case there is an error contacting upstream
-          maybeStaleCache = data;
+          return;
         }
       }
+
+      // Check to see if the course has a cached value in Firestore
+      const firestoreCacheItem = await getCacheItemFromFirestore(courseID);
+      if (firestoreCacheItem !== null) {
+        const age = getCacheItemAge(firestoreCacheItem);
+        if (age < getCacheItemExpirationSeconds(firestoreCacheItem)) {
+          // Use this log line to construct custom metrics
+          // so we can observe how often the Firestore cache is used:
+          // https://cloud.google.com/logging/docs/logs-based-metrics#user-metrics
+          functions.logger.info("Using Firestore cached data for course", {
+            course_id: courseID,
+            age,
+          });
+
+          await sendCachedResponse(response, {
+            data: await decompressBody(firestoreCacheItem.d),
+            contentType: firestoreCacheItem.c,
+            status: firestoreCacheItem.s,
+            age,
+          });
+
+          return;
+        }
+      }
+
+      const maybeStale = firestoreCacheItem ?? globalCacheItem;
 
       // Fetch the upstream data
       const encodedCourseID = encodeURIComponent(courseID);
       const url = `${UPSTREAM_COURSE_DATA_URL}?courseID=${encodedCourseID}`;
       let upstreamResponse: fetch.Response;
       let upstreamResponseBody: string;
+      let latencyMs: number;
       try {
+        const start = process.hrtime();
         upstreamResponse = await fetch.default(url, {
           method: "GET",
           headers: { "User-Agent": "gt-scheduler-cache-proxy (node-fetch)" },
         });
         upstreamResponseBody = await upstreamResponse.text();
+        const stop = process.hrtime(start);
+
+        latencyMs = (stop[0] * 1e9 + stop[1]) / 1e6;
       } catch (err) {
         // There was an error contacting the upstream API
         // (this is not a 3xx-5xx response code).
         // Log the error
+        // Use this log line to construct custom metrics
+        // so we can observe how often the Course Critique API is down:
+        // https://cloud.google.com/logging/docs/logs-based-metrics#user-metrics
         functions.logger.error(
           `An error occurred while making a request to Course Critique: ${err}`,
           { url }
         );
 
         // If there is cached data (even if stale), return it.
-        if (maybeStaleCache !== null) {
-          await sendCachedResponse(courseID, response, maybeStaleCache);
+        if (maybeStale !== null) {
+          await sendCachedResponse(response, {
+            data: await decompressBody(maybeStale.d),
+            contentType: maybeStale.c,
+            status: maybeStale.s,
+            age: getCacheItemAge(maybeStale),
+          });
+
           return;
         }
 
@@ -140,50 +179,101 @@ export const getCourseDataFromCourseCritique = functions.https.onRequest(
         return;
       }
 
-      // Store the timestamp, data, and status code in Firestore
-      const timestamp = new Date().toISOString();
+      // Use this log line to construct custom metrics
+      // so we can observe the latency and response code
+      // of Course Critique API requests:
+      // https://cloud.google.com/logging/docs/logs-based-metrics#user-metrics
+      functions.logger.info("Made upstream request to Course Critique API", {
+        course_id: courseID,
+        latency_ms: latencyMs,
+        status: upstreamResponse.status,
+      });
+
+      // Eagerly send the response before storing the cache item
       const contentType =
         upstreamResponse.headers.get("content-type") ?? "application/json";
-      const writePromise = storeCacheItem(courseID, upstreamResponseBody, {
+      sendCachedResponse(response, {
+        data: upstreamResponseBody,
+        contentType,
+        status: upstreamResponse.status,
+        age: 0,
+      });
+
+      // Store the timestamp, data, and status code
+      // in both Firestore & the global cache
+      const timestamp = new Date().toISOString();
+      await storeCacheItem(courseID, upstreamResponseBody, {
         v: 1,
         t: timestamp,
         s: upstreamResponse.status,
         c: contentType,
       });
-
-      functions.logger.info("Loaded cache with fresh data for course", {
-        course_id: courseID,
-      });
-
-      // Eagerly send the response before waiting for the write to complete
-      response
-        .status(upstreamResponse.status)
-        .header("Content-Type", contentType)
-        .header("Age", "0")
-        .send(upstreamResponseBody);
-
-      await writePromise;
     })
 );
 
-async function sendCachedResponse(
-  courseID: string,
-  response: Response,
-  item: CacheItem
-) {
-  const uncompressedBody = await decompress(Buffer.from(item.d, "base64"));
-  const now = new Date();
-  const age = Math.max(0, date.subtract(now, new Date(item.t)).toSeconds());
-  response
-    .status(item.s)
-    .header("Content-Type", item.c)
-    .header("Age", String(Math.floor(age)))
-    .send(uncompressedBody);
+function getCacheItemFromGlobalCache(courseID: string): CacheItem | null {
+  const item = globalCache[courseID];
+  if (item == null) return null;
+  return item;
+}
 
-  functions.logger.info("Using cached data for course", {
-    course_id: courseID,
-    age,
-  });
+async function getCacheItemFromFirestore(
+  courseID: string
+): Promise<CacheItem | null> {
+  const document = await courseDataCacheCollection.doc(courseID).get();
+  if (document.exists) {
+    const data = document.data();
+    if (data != null) {
+      return data;
+    }
+  }
+
+  return null;
+}
+
+const compress = util.promisify(zlib.brotliCompress);
+const decompress = util.promisify(zlib.brotliDecompress);
+
+async function compressBody(body: string): Promise<string> {
+  return (await compress(body)).toString("base64");
+}
+
+async function decompressBody(compressed: string): Promise<Buffer> {
+  return await decompress(Buffer.from(compressed, "base64"));
+}
+
+function getCacheItemAge(cacheItem: CacheItem): number {
+  const now = new Date();
+  const timestamp = new Date(cacheItem.t);
+  const age = Math.max(0, date.subtract(now, timestamp).toSeconds());
+  return Math.floor(age);
+}
+
+function getCacheItemExpirationSeconds(cacheItem: CacheItem): number {
+  // Use a different expiration if the cached response is an error
+  if (cacheItem.s >= 400) {
+    return CACHE_ERROR_RESPONSE_EXPIRATION_SECONDS;
+  } else {
+    return CACHE_SUCCESS_RESPONSE_EXPIRATION_SECONDS;
+  }
+}
+
+type PreparedCacheResponseData = {
+  data: string | Buffer;
+  contentType: string;
+  status: number;
+  age: number;
+};
+
+function sendCachedResponse(
+  response: Response,
+  data: PreparedCacheResponseData
+) {
+  response
+    .status(data.status)
+    .header("Content-Type", data.contentType)
+    .header("Age", String(Math.floor(data.age)))
+    .send(data.data);
 }
 
 async function storeCacheItem(
@@ -191,11 +281,11 @@ async function storeCacheItem(
   rawBody: string,
   item: Omit<CacheItem, "d">
 ): Promise<void> {
-  const compressedBody = (await compress(rawBody)).toString("base64");
   const newDocument: CacheItem = {
     ...item,
-    d: compressedBody.toString(),
+    d: await compressBody(rawBody),
   };
 
+  globalCache[courseID] = newDocument;
   await courseDataCacheCollection.doc(courseID).set(newDocument);
 }

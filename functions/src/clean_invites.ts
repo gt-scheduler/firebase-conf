2def951e333157d80d85905737aa5ff063afc869
Yatharth Bhargava <yatharthbhargava@gmail.com>
2023-03-01 17:06:37 -0500

    remove invite from sender's schedule if expired

--- functions/src/clean_invites.ts
+++ functions/src/clean_invites.ts
@@ -1,11 +1,13 @@
 import admin from "./firebase";
-import { FriendInviteData } from "../utils/types";
+import { AnyScheduleData, FriendInviteData, Version3ScheduleData } from "../utils/types";
 import * as functions from "firebase-functions";
 
 const firestore = admin.firestore();
 const invitesCollection = firestore.collection(
   "friend-invites"
 ) as FirebaseFirestore.CollectionReference<FriendInviteData>;
+
+const schedulesCollection = firestore.collection("schedules") as FirebaseFirestore.CollectionReference<AnyScheduleData>
 
 /*
 This function is called every week to clean up the friend-invites collection. We only want to keep the invites made in the past week.
@@ -25,6 +27,14 @@ export const cleanInvites = functions.pubsub
         (new Date().getTime() - data.created.toDate().getTime()) /
         (1000 * 3600 * 24);
       if (diffInDays >= 7) {
+        const senderDoc = await schedulesCollection.doc(data.sender).get();
+        const senderData = await senderDoc.data() as Version3ScheduleData | undefined;
+        if (!senderData) {
+          doc.ref.delete();
+          return;
+        }
+        delete senderData.terms[data.term].versions[data.version].friends[data.friend];
+        await senderDoc.ref.set(senderData);
         doc.ref.delete();
       }
     });

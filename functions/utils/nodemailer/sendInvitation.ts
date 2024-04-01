4bb7a37945bec5be22ced18848583234def4d477
Yatharth Bhargava <yatharthbhargava@gmail.com>
2024-04-01 03:04:55 -0400

    email styling

--- functions/utils/nodemailer/sendInvitation.ts
+++ functions/utils/nodemailer/sendInvitation.ts
@@ -41,32 +41,81 @@ export default async function sendInvitation({
   Accept the invite: ${inviteUrl}
   `;
   const html = `
-    <div style="width:600px;">
-      <div class="header" style="background-image:linear-gradient(to right, #FF5B54, #FFBA4E); height:88px; 
-      font-weight:bold; color:white; font-size:48px; padding-left: 11px;
-      padding-top: 10px;">GT Scheduler</div>
-      <div class="body-text" style="background-color:#222222; padding:20px;">
-        <h1 style="color: white;
-        font-size: 24px;">Invitation to View Schedule</h1>  
-        <p style="color: white;">Hello,</p>
-        <p style="color: white;">You have been invited to a GT schedule by <a href="mailto:${senderEmail}" style="color:#1456D3">${senderEmail}</a> 
-        to import their schedule "<b>${versionNames.join(
-          ", "
-        )}</b>" for the ${semester}. Accepting this invite
-        allows you to compare this schedule to others in you Shared Schedule panel when turned on. 
-        You can always remove this schedule from your view in the site after importing it if you 
-        chose to do so.</p>
-        <p style="color: white;">Please click the link below to accept this invitation.</p>
-        <a href="${inviteUrl}" style="color:#FE5B53">${inviteUrl}</a>
-        <p style="color: white;">For any inquiries, please contact us at <a href="contact@gt-scheduler.org" style="color:#FE5B53">contact@gt-scheduler.org</a></p>
-        <p style="color: white;">The GT Scheduler Team @<b>Bits of Good</b></p>
-        <div style="display: flex;flex-direction: row;justify-content: center;
-        color: white;font-size: 24px;">
-          <img src="https://drive.google.com/file/d/1VwCN1aqj3NoC1w5xnvSQ__jc1wCqQuHF/view?usp=share_link" alt="Bog Logo" style="width:18px; height:24px;">
-          <p>bits of good</p>
-        </div>
-        </div>
+  <div style="width: 600px">
+    <div
+      class="header"
+      style="
+        background-image: linear-gradient(to right, #ff5b54, #ffba4e);
+        height: 88px;
+        font-weight: bold;
+        color: white;
+        font-size: 48px;
+        padding-left: 11px;
+        padding-top: 10px;
+      "
+    >
+      GT Scheduler
     </div>
+    <div class="body-text" style="background-color: #222222; padding: 20px">
+      <h1 style="color: white; font-size: 24px">Invitation to View Schedule</h1>
+      <p style="color: white">Hello,</p>
+      <p style="color: white">
+        You have been invited to a GT schedule by
+        <a
+          href="mailto:${senderEmail}"
+          style="color: #1456d3; text-decoration: underline"
+          ><b>${senderEmail}</b></a
+        >
+        to import the following schedules: <b>${versionNames.join(", ")}</b> for
+        the ${semester}. Accepting this invite allows you to compare this schedule
+        to others in you Shared Schedule panel when turned on. You can always
+        remove this schedule from your view in the site after importing it if you
+        chose to do so.
+      </p>
+      <a
+        href="${inviteUrl}"
+        style="color: white; text-decoration: none; width: 100%"
+      >
+        <div
+          style="
+            background: #fe5b53;
+            border-radius: 8px;
+            padding: 10px;
+            text-align: center;
+          "
+        >
+          <b> Accept Invite</b>
+        </div>
+      </a>
+      <p style="color: white">
+        For any inquiries, please contact us at
+        <a
+          href="mailto:contact@gt-scheduler.org"
+          style="color: #fe5b53; text-decoration: none"
+          >contact@gt-scheduler.org</a
+        >
+      </p>
+      <p style="color: white">The GT Scheduler Team @<b>Bits of Good</b></p>
+      <div
+        style="
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          align-items: center;
+          color: white;
+          font-size: 24px;
+        "
+      >
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/gt-scheduler-web-prod.appspot.com/o/bogLogo.png?alt=media&token=cda9f3c3-8c90-460c-8a6a-36d02f542066"
+          alt="Bog Logo"
+          style="width: 28px; height: 30px"
+        />
+        <p>bits of good</p>
+      </div>
+    </div>
+  </div>
+
   `;
   await sendEmail(friendEmail, subject, text, html);
 }

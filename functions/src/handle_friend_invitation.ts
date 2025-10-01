08bfcffa9b09548e13229906e5998543d91f721e
emilyliublair <67622097+emilyliublair@users.noreply.github.com>
2025-10-01 11:31:00 -0400

    Fix and Migrate Email Service to Juno (#19)
    * fix email
    
    * lint + format
    
    * address comments

--- functions/src/handle_friend_invitation.ts
+++ functions/src/handle_friend_invitation.ts
@@ -85,10 +85,10 @@ export const handleFriendInvitation = functions
         Object.keys(senderSchedule.terms).forEach((t) => {
           Object.keys(senderSchedule.terms[t].versions).forEach((v) => {
             if (!senderSchedule.terms[t].versions[v].friends) {
-              senderSchedule.terms[t].versions[v].friends = {}
+              senderSchedule.terms[t].versions[v].friends = {};
             }
-          })
-        })
+          });
+        });
 
         // Check if link hasn't expired by calculating the difference between the current time and the time the link was created
         const defaultValidDuration = 7 * 24 * 60 * 60;
@@ -199,7 +199,7 @@ export const handleFriendInvitation = functions
           term: inviteData.term,
         });
       } catch (err) {
-        console.log(err)
+        functions.logger.log(err);
         return response.status(400).json(apiError("unkown-error"));
       }
     });
